0d4451886307c1cc0dc4a3897f960cfbc59a5082
james-brown98 <james.brown9809@hotmail.co.nz>
2017-03-03 12:36:13 +1300

    add placeholders for menu.html and css

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var path = require('path')
 var express = require('express')
 var hbs = require('express-handlebars')
-var tape = require('tape')
+
 var routes = require('./routes')
-var fs = require('fs')
 
 var app = express()
 module.exports = app
 
+
+// Middleware
+var hbs = require('express-handlebars')
 app.engine('hbs', hbs({
     extname: 'hbs',
     defaultLayout: 'main'
   }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
-
-// Routes
-app.get('/menu', routes.menu)
