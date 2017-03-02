6e8e9a9d4017e139e8b1b6451cbc77739a9bd6ef
DanBlakeNZ <dblakenz@hotmail.com>
2017-03-03 11:00:33 +1300

    npm test working

--- tests/DanBtest.js
+++ tests/DanBtest.js
@@ -5,11 +5,3 @@ test('test setup working', function (t) {
   t.pass()
   t.end()
 })
-
-test('test say hi', function (t) {
-  var message = "hi"
-  var expected = "hi"
-  var actual = routes.sayhi(message)
-  t.equal(actual, expected)
-  t.end()
-})
