551f383659b8f2b76f592ab8e6068d9b84a8ec56
DanBlakeNZ <dblakenz@hotmail.com>
2017-03-03 12:36:44 +1300

    simple order array function tested and passed

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 var db = require ('./database/db')
 
 module.exports = {
-  menu:menu
+  menu:menu,
+  addItem:addItem
 }
 
 function menu (req, res) {
@@ -10,10 +11,24 @@ function menu (req, res) {
   res.render('menu', data)
 }
 
+
+
 //create variable that will store the orders array [name, price, quantity]
-//create click event for menu items
+var orderedItems = [ [""] ]
+
+//create click event for menu items that runs addItem function
+
+function addItem(itemName, itemPrice){
+  if (orderedItems[0] == ""){
+    orderedItems[0] = ([itemName,itemPrice])
+  }
+  return orderedItems
+}
+
+
+
 //on click of menu items
-  //loop through menu array
+  //loop through order items array
     //if menu item name exists in the array
       // item quantity +1
     //else add item to the menu array
--- tests/DanBtest.js
+++ tests/DanBtest.js
@@ -5,3 +5,15 @@ test('test setup working', function (t) {
   t.pass()
   t.end()
 })
+
+test('test adding item', function (t) {
+  var itemName = 'coffee'
+  var itemPrice = 3.50
+
+  var expected = [
+    ['coffee', 3.50]
+  ]
+  var actual = routes.addItem(itemName, itemPrice)
+  t.deepEquals(actual, expected)
+  t.end()
+})
