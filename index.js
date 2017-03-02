d1bd595eae6075a04f47f21abedf2f454d6ef615
daniel-reason <dreason@live.co.uk>
2017-03-02 17:05:32 +1300

    ste up variables on server.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+
+var server = require('./server')
+
 var port = process.env.PORT || 3000
+
+server.listen(PORT, function () {
+  console.log('Server is listening on port', PORT)
+})
---
+++ server.js
@@ -0,0 +1,10 @@
+var path = require('path')
+var express = require('express')
+var hbs = require('express-handlebars')
+var tape = require('tape')
+var routes = require('./routes')
+var fs = require('fs')
+
+var app = express()
+
+module.exports = app
