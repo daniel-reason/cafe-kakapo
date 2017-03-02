7c25560d4d105f1f3d7a62f3548aa3bf8a9378a6
DanBlakeNZ <dblakenz@hotmail.com>
2017-03-03 11:48:54 +1300

    added sudo code

--- routes.js
+++ routes.js
@@ -9,3 +9,17 @@ function menu (req, res) {
   data.showItems = req.query.showItems
   res.render('menu', data)
 }
+
+//create variable that will store the orders array [name, price, quantity]
+//create click event for menu items
+//on click of menu items
+  //loop through menu array
+    //if menu item name exists in the array
+      // item quantity +1
+    //else add item to the menu array
+  //once the loop has finished update subtotal
+
+  //create subtotal variable
+  //create subtotal function
+    //loop through menu array and get price
+      //subtotal += price x quantity
